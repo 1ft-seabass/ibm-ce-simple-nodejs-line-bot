e19e7ffdc0ed2e32816babf4f04b813df4cedf4b
Tanaka Seigo <seigo@1ft-seabass.jp>
2024-03-06 22:00:33 +0900

    更新

--- server.js
+++ server.js
@@ -17,17 +17,8 @@ app.use(express.static(__dirname + '/public'));
 
 // 実際にメッセージを受け付ける
 app.post('/webhook', line.middleware(config), (req, res) => {
-  console.log('/webhook');
-
     console.log(req.body.events);
-
-    // ここの if 文は developer console の「接続確認」用なので削除して問題ないです。
-    if(req.body.events[0].replyToken === '00000000000000000000000000000000' && req.body.events[1].replyToken === 'ffffffffffffffffffffffffffffffff'){
-        res.send('Hello LINE BOT!(POST)');
-        console.log('疎通確認用');
-        return; 
-    }
-
+    
     Promise
       .all(req.body.events.map(handleEvent))
       .then((result) => res.json(result));
@@ -36,16 +27,19 @@ app.post('/webhook', line.middleware(config), (req, res) => {
 const client = new line.messagingApi.MessagingApiClient(config);
 
 async function handleEvent(event) {
-  console.log('/handleEvent');
 
   if (event.type !== 'message' || event.message.type !== 'text') {
     return Promise.resolve(null);
   }
-
-  return client.replyMessage(event.replyToken, {
-    type: 'text',
-    text: event.message.text // 実際に返信の言葉を入れる箇所（そのままオウム返し）
+  
+  return client.replyMessage({
+    replyToken: event.replyToken,
+    messages: [{
+      type: 'text',
+      text: event.message.text // 実際に返信の言葉を入れる箇所（そのままオウム返し）
+    }],
   });
+  
 }
 
 app.listen(PORT);
